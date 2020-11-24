3baaab96728a98f0e918b93bad9b1157af1a331b
Evan Baker <ebake7@aexp.com>
2020-11-24 17:54:10 -0500

    improve image rendering

--- components/StoryBlock.js
+++ components/StoryBlock.js
@@ -16,13 +16,11 @@ const StoryBlock = ({ text, image, id }) => {
         <>
             <Waypoint onEnter={onEnter} onLeave={onLeave} key={id} />
             <div className="h-screen flex content-start mb-px" >
-                {text && <p className="mb-auto ml-auto mr-auto p-10 z-10">{text}</p>}
+                {text && <p className="mb-auto ml-auto mr-auto p-10 z-10 shadow-sm">{text}</p>}
             </div>
-            {displayImage && (
-                <div className={`${displayImage ? 'fixed' : 'none'} top-0 bottom-0 left-0 right-0 z-0`}>
-                    <Image src={`https:${image.url}`} layout="fill" className="object-cover" />
-                </div>
-            )}
+            {image.url && (<div className={`fixed ${displayImage ? 'opacity-100' : 'opacity-0'} transition-opacity duration-1000 top-0 bottom-0 left-0 right-0 z-0`}>
+                <Image src={`https:${image.url}`} layout="fill" className="object-cover" />
+            </div>)}
         </>
     );
 };
