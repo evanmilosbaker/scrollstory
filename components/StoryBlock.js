ddf1d0921526535f004438403dd0f85bb8abfa85
Evan Baker <ebake7@aexp.com>
2020-11-25 13:14:46 -0500

    add background opacity to text

--- components/StoryBlock.js
+++ components/StoryBlock.js
@@ -16,7 +16,7 @@ const StoryBlock = ({ text, images, id }) => {
         <>
             <Waypoint onEnter={onEnter} onLeave={onLeave} key={id} />
             <div className="h-screen flex content-start mb-px" >
-                {text && <p className="mb-auto ml-auto mr-auto p-10 z-10 shadow-sm" dangerouslySetInnerHTML={{__html: text}} />}
+                {text && <p className="mb-auto ml-auto mr-auto pl-10 pr-10 z-10 shadow-sm bg-white bg-opacity-50" dangerouslySetInnerHTML={{__html: text}} />}
             </div>
             {images[0] && images[0].url && (<div className={`fixed ${displayImage ? 'opacity-100' : 'opacity-0'} transition-opacity duration-1000 top-0 bottom-0 left-0 right-0 z-0`}>
                 <Image src={`https:${images[0].url}`} layout="fill" className="object-cover" />
